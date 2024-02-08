ebd693929490451c7110c10c95d98afc54ab9eb1
SueCal <159279790+SueCal@users.noreply.github.com>
2024-02-08 03:35:53 +0100

    Update streamlit_app.py
    Added multi-select for my_fruit_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,9 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+# Let's put a picklist here, so they can pick the fruits they want to include
+streamlit.multielect("Pick some fruit:", list(my_fruit_list.index))
+
+# display the table on the page
+streamlit.dataframe(my_fruit_list)
