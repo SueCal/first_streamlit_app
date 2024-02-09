81ce8aea9cef20b4352b8bc3203157712feba1bd
SueCal <159279790+SueCal@users.noreply.github.com>
2024-02-09 01:50:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,14 +36,11 @@ try:
       streamlit.error("Please select a fruit to get information.")
    else:
       back_from_function = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe(back_from_function)   
-  # except URLError as e:
-  # streamlit.error()
-
-  # Allow the enduser to add a fruit to the list
-  # add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
-  # streamlit.write('Thanks for adding ', add_my_fruit)
-
+      streamlit.dataframe(back_from_function)  
+      
+   except URLError as e:
+      streamlit.error()
+      
 streamlit.stop()
 
 # import snowflake.connector
